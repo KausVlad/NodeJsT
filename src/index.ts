818b6d7ff175af0701b0111d4a1d23bb7504048d
KausVlad <kausvirtual@gmail.com>
2023-05-31 03:09:20 +0300

    add specificData

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,20 @@ const getRandomNumber = (min: number, max: number): number => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-const getDataStructure = (): [number, ...TypeProduct[]] => {
-  const creationTime = new Date().getTime();
-  const dataStructure: [number, ...TypeProduct[]] = [creationTime];
+const getTime = (): string => {
+  const timestamp = new Date().getTime();
+  const date = new Date(timestamp);
+
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
+
+  return `${hours}:${minutes}:${seconds}`;
+};
+
+const getDataStructure = (): [string, ...TypeProduct[]] => {
+  const creationTime = getTime();
+  const dataStructure: [string, ...TypeProduct[]] = [creationTime];
   for (let i = 1; i <= 100; i++) {
     const productName = `Product ${i}`;
     const price = getRandomNumber(10, 100);
@@ -20,5 +31,24 @@ const getDataStructure = (): [number, ...TypeProduct[]] => {
   }
   return dataStructure;
 };
+const dataStructure = getDataStructure();
 
-getDataStructure();
+console.log(`Creation time of data structure: ${dataStructure[0]}`);
+
+const getSpecificData = (
+  dataId: number
+): [string, string | TypeProduct, number] => {
+  return [getTime(), dataStructure[dataId], dataId];
+};
+
+const specificData = getSpecificData(50);
+
+console.log(
+  `Call time of element number ${specificData[2]} : ${
+    specificData[0]
+  } \nThis element has the following parameters: ${
+    typeof specificData[1] === 'object' ? specificData[1].productName : ''
+  } with price ${
+    typeof specificData[1] === 'object' ? specificData[1].price : ''
+  }$ `
+);
