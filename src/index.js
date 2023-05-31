03656d6cd27bbfd747b27ede57e078afe154a9e5
KausVlad <kausvirtual@gmail.com>
2023-05-31 23:03:42 +0300

    its work

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
 let interval = setInterval(() => {
-  console.log('(1 )');
+  console.log('(1 6)');
   clearInterval(interval);
 });
 
-setImmediate(() => console.log('(2 )'));
+setImmediate(() => console.log('(2 9)'));
 
-setTimeout(() => console.log('(3 )'));
+setTimeout(() => console.log('(3 7)'));
 
-process.nextTick(() => console.log('(4 )'));
+process.nextTick(() => console.log('(4 5)'));
 
-console.log('(5 )');
+console.log('(5 1)');
 
 let myPromise = () =>
   new Promise((resolve) =>
     setTimeout(() => {
-      console.log('(6 )');
+      console.log('(6 8)');
       resolve();
     })
   );
 
 let myPromise2 = () =>
   new Promise((resolve) => {
-    console.log('(7 )');
+    console.log('(7 3)');
     resolve();
   });
 
-myPromise().then(console.log('(8 )'));
+myPromise().then(console.log('(8 2)'));
 
-myPromise2().then(console.log('(9 )'));
+myPromise2().then(console.log('(9 4)'));
